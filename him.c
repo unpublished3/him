e26945f842fe0f71ef4b434499079e92f3b6e291
unpublished3 <chapagainsakshyam@gmail.com>
2026-01-11 06:59:16 +0545

    Initialize rows with window sizes

--- him.c
+++ him.c
@@ -5,7 +5,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/_types/_ssize_t.h>
+#include <sys/ioctl.h>
+#include <sys/ttycom.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -14,25 +17,33 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+  struct termios orig_termios;
+  int screenRows;
+  int screenCols;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
     die("tcgetattr");
   atexit(disableRawMode);
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -53,6 +64,17 @@ char editorReadKey() {
   return c;
 }
 
+int getWidowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  }
+  *rows = ws.ws_row;
+  *cols = ws.ws_row;
+  return 0;
+}
+
 /*** input ***/
 
 void processEditorKeypress() {
@@ -60,24 +82,41 @@ void processEditorKeypress() {
 
   switch (c) {
   case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
   }
 }
 
-/*** outpue ***/
+/*** output ***/
 void editorClearScreen() { write(STDOUT_FILENO, "\x1b[2J", 4); }
 
 void editorRepositionCursor() { write(STDOUT_FILENO, "\x1b[;H", 4); }
 
+void editorDrawRows() {
+  int y;
+  char printChar[16];
+  for (y = 0; y < E.screenRows; y++) {
+    sprintf(printChar, "%d\r\n", y);
+    write(STDOUT_FILENO, printChar, strlen(printChar));
+  }
+}
+
 /*** init ***/
+void initEditor() {
+  if (getWidowSize(&E.screenRows, &E.screenRows) == -1)
+    die("getWindowSize");
+}
 
 int main() {
   enableRawMode();
+  initEditor();
 
   while (1) {
     editorRepositionCursor();
     editorClearScreen();
+    editorDrawRows();
     processEditorKeypress();
   }
 
