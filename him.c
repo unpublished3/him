8435c68cb71513e2ddce8fab9c6c7464cfe31b36
unpublished3 <chapagainsakshyam@gmail.com>
2026-01-14 07:40:24 +0545

    Fallback for ioctl && fix incorrect col assignment

--- him.c
+++ him.c
@@ -64,14 +64,39 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+
+  return 0;
+}
+
 int getWidowSize(int *rows, int *cols) {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+      return -1;
+    return getCursorPosition(rows, cols);
   }
   *rows = ws.ws_row;
-  *cols = ws.ws_row;
+  *cols = ws.ws_col;
   return 0;
 }
 
@@ -105,7 +130,7 @@ void editorDrawRows() {
 
 /*** init ***/
 void initEditor() {
-  if (getWidowSize(&E.screenRows, &E.screenRows) == -1)
+  if (getWidowSize(&E.screenRows, &E.screenCols) == -1)
     die("getWindowSize");
 }
 
